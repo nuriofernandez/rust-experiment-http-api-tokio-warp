9ed5c40b063bfbc6b8d92719aaddb692d7fa6b4b
Nurio Fernández <personal@nurio.me>
2021-06-27 15:37:03 +0200

    Added /bye/{} api method

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
+    // GET /bye/nurio => 200 OK with body "Bye, nurio!"
+    let bye = warp::path!("bye" / String)
+        .map(|name| format!("Bye, {}!", name));
+
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
         .await;
